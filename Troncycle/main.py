6e47f1540ac56505ece2cb05cb84c12f4efee0ba
yuval-p <p.yuval@gmail.com>
2015-04-08 17:32:36 +0300

    1. bug fix 2. new menu 3. quit and return to menu

--- Troncycle/main.py
+++ Troncycle/main.py
@@ -5,9 +5,21 @@ import time
 
 pygame.init()
 
+
+#settings
+screenSize = [1000,1000]
+numOfTurns = 5
+player1Color = [(0, 0, 255),(255, 0,0)]
+player2Color = [(255, 0,0),(0, 0, 255)]
+
+
+END_GAME = 1
+QUIT = 2
+NEW_GAME = 3
+
 myfont = pygame.font.SysFont("monospace", 30)
 
-screen = pygame.display.set_mode([1000, 1000])
+screen = pygame.display.set_mode(screenSize)
 
 # TODO change to the sensors input..
 def keyPress(sensor1,sensor2):
@@ -31,15 +43,21 @@ def keyPress(sensor1,sensor2):
         sensor2.setSpeed(-1)
 
 
-def findCollision(bikePosition,bike):
+def findCollision(bikePosition,bike,rival):
     if bike.prev_pos.__len__()-20 < 0:
         maxPos = 0
+    elif rival:
+        maxPos = bike.prev_pos.__len__()
     else:
         maxPos = bike.prev_pos.__len__()-20
+
+    if bikePosition[0] < 0 or bikePosition[0] > screenSize[0] or bikePosition[1] < 0 or bikePosition[1] > screenSize[1]:
+        return True
+
     for p in bike.prev_pos[:maxPos]:
         if abs(abs(p[0])-abs(bikePosition[0])) < 5 and abs(abs(p[1])-abs(bikePosition[1])) < 5:
-         #   print("collision!!!!")
             return True
+
     return False
 
 class Sensors():
@@ -64,20 +82,20 @@ class Sensors():
 
 
 class Bike():
-    def __init__(self,color1,color2,playerNum,sensors):
-        self.xdir = random.uniform(1,500)
-        self.ydir = random.uniform(1,500)
+    def __init__(self,colors,sensors):
+        self.xdir = random.uniform(1,screenSize[0])
+        self.ydir = random.uniform(1,screenSize[1])
         self.prev_pos = []
-        self.headcolor = color1 #(0, 0, 255)
-        self.bodydcolor = color2 #(255, 0, 0)
+        self.headcolor = colors[0]
+        self.bodydcolor = colors[1]
         self.sensors = sensors
         self.score = 0
 
     def reset(self):
         self.prev_pos = []
         self.sensors.reset()
-        self.xdir = random.uniform(1,500)
-        self.ydir = random.uniform(1,500)
+        self.xdir = random.uniform(1,screenSize[0])
+        self.ydir = random.uniform(1,screenSize[1])
 
     def getPosition(self):
         return (self.xdir,self.ydir)
@@ -92,32 +110,33 @@ class Bike():
         self.xdir += self.sensors.speed * math.cos(math.radians(self.sensors.angle))
         self.ydir += self.sensors.speed * math.sin(math.radians(self.sensors.angle))
 
+def getTextSize(text):
+    return myfont.size(text)
+
 
 def game(bike1,bike2):
     clock = pygame.time.Clock()
     gameover = False
+    roundNum = 0
     while True:
         clock.tick(20)
         pygame.mouse.set_visible(False)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
-        if bike1.xdir >= screen.get_width():
-          print("collision!")
-        if bike1.ydir >= screen.get_width():
-          print("collision!")
 
         bgi = pygame.image.load('BlackBackground.gif')
         screen.blit(bgi, (0, 0))
         if not gameover:
+
             keyPress(bike1.sensors,bike2.sensors)
             bike1.update()
             bike2.update()
 
             Col = False
             # find if player1 collides with player2 or its own track
-            Col = Col or  findCollision(bike1.getPosition(),bike1)
-            Col = Col or findCollision(bike1.getPosition(),bike2)
+            Col = Col or  findCollision(bike1.getPosition(),bike1,False)
+            Col = Col or findCollision(bike1.getPosition(),bike2,True)
 
             if(Col):
                 print("player1 lose")
@@ -127,8 +146,8 @@ def game(bike1,bike2):
 
             Col = False
         # find if player2 collides with player1 or its own track
-            Col = Col or findCollision(bike2.getPosition(),bike1)
-            Col = Col or findCollision(bike2.getPosition(),bike2)
+            Col = Col or findCollision(bike2.getPosition(),bike1,True)
+            Col = Col or findCollision(bike2.getPosition(),bike2,False)
 
             if(Col):
                 print("player2 lose")
@@ -137,28 +156,78 @@ def game(bike1,bike2):
                 loose = 2
 
         if gameover:
-            loseText = myfont.render("player"+ loose.__str__()+" lose", 1, (255,255,0))
-            screen.blit(loseText, (500, 100))
-            press2Cont = myfont.render("Press space to continue", 1, (255,255,0))
-            screen.blit(press2Cont, (500, 500))
+
+            loseText = "player"+ loose.__str__()+" lose"
+            loseText_X_position = screenSize[0]/2 - (getTextSize(loseText))[0]/2
+            screen.blit(myfont.render(loseText,1,(255,255,0)), (loseText_X_position, 300))
+
+            press2Cont ="Press space to continue"
+            press2Cont_X_possition = screenSize[0]/2 - (getTextSize(press2Cont))[0]/2
+            screen.blit(myfont.render(press2Cont,1,(255,255,0)), (press2Cont_X_possition, 500))
+
             if pygame.key.get_pressed()[pygame.K_SPACE]:
                 gameover = False
                 bike1.reset()
                 bike2.reset()
+                roundNum+=1
+                if roundNum >= numOfTurns:
+                    return END_GAME
 
-        player1Score = myfont.render("player1 score:"+ bike1.score.__str__(), 1, (255,100,0))
-        screen.blit(player1Score, (100, 20))
-        player2Score = myfont.render("player2 score:"+ bike2.score.__str__(), 1, (255,100,0))
-        screen.blit(player2Score, (700, 20))
+
+        player1Score = "player1 score:"+ bike1.score.__str__()
+        player1Score_X_possition = screenSize[0]/3 - (getTextSize(player1Score))[0]/2-150
+        screen.blit(myfont.render(player1Score,1,(255,100,0)), (player1Score_X_possition, 20))
+
+        player2Score = "player2 score:"+ bike2.score.__str__()
+        player2Score_X_possition = 2*screenSize[0]/3 - (getTextSize(player2Score))[0]/2+150
+        screen.blit(myfont.render(player2Score,1,(255,100,0)), (player2Score_X_possition, 20))
+
+        roundNumText = "round: "+ (roundNum+1).__str__()
+        roundNumText_X_possition = screenSize[0]/2 - (getTextSize(roundNumText))[0]/2
+        screen.blit(myfont.render(roundNumText,1,(155,100,30)), (roundNumText_X_possition, 100))
+
+
+        if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+            return QUIT
+
+
 
         pygame.display.flip()
 
 
 
+def menu():
+    pygame.mouse.set_visible(True)
+    bgi = pygame.image.load('BlackBackground.gif')
+    screen.blit(bgi, (0, 0))
+    pygame.display.flip()
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(20)
+        startText = "START NEW GAME!!!"
+        startText_X_possition = screenSize[0]/2 - (getTextSize(startText))[0]/2
+        sng = screen.blit(myfont.render(startText,1,(155,100,30)), (startText_X_possition, 100))
+
+        for ev in pygame.event.get():
+            if ev.type == pygame.MOUSEBUTTONUP:
+                if sng.collidepoint(pygame.mouse.get_pos()):
+                    return NEW_GAME
+        bgi = pygame.image.load('BlackBackground.gif')
+        pygame.display.flip()
+
+
 def main():
-    bike1 = Bike((0, 0, 255),(255, 0,0),1,Sensors())
-    bike2 = Bike((255, 0,0),(0, 0, 255),2,Sensors())
-    game(bike1,bike2)
+
+    while True:
+        ret = menu()
+        #bgi = pygame.image.load('BlackBackground.gif')
+
+        if ret == NEW_GAME:
+            bike1 = Bike(player1Color,Sensors())
+            bike2 = Bike(player2Color,Sensors())
+            var = game(bike1,bike2)
+        #elif ret =
+
 
     pygame.mouse.set_visible(True)
 
