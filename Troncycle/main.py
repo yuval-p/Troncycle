0b41a94e9b5c5f094b895af81f68d9178e313303
idan <farinidan@gmail.com>
2015-04-08 14:30:21 +0300

    1
    1

--- Troncycle/main.py
+++ Troncycle/main.py
@@ -4,7 +4,8 @@ import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((1000,1003))
+screen = pygame.display.set_mode((1000, 1003))
+
 
 class LightBike(pygame.sprite.Sprite):
     def __init__(self):
@@ -16,70 +17,80 @@ class LightBike(pygame.sprite.Sprite):
         self.x = 0.0
         self.y = 0.0
         self.angel = 0
-        
+
     def update(self):
 
         self.prev_pos.append(self.rect.center)
         if len(self.prev_pos) > 1:
-            pygame.draw.aalines(screen,(0,0,255),False,self.prev_pos)
-             
-        #if len(self.prev_pos) > 100:
-         #   self.prev_pos.pop(0)
-            
-    #    if pygame.key.get_pressed()[pygame.K_RIGHT]
-                
+            pygame.draw.aalines(screen, (0, 0, 255), False, self.prev_pos)
+
+            # if len(self.prev_pos) > 100:
+            #   self.prev_pos.pop(0)
+
+            #    if pygame.key.get_pressed()[pygame.K_RIGHT]
+
         self.image = pygame.image.load('cycle.gif')
-                    
+
         if pygame.key.get_pressed()[pygame.K_DOWN]:
-                self.slowdown()
-            #    self.image = pygame.image.load('light4.gif')
+            self.slowdown()
+            # self.image = pygame.image.load('light4.gif')
         elif pygame.key.get_pressed()[pygame.K_LEFT]:
-                self.setAngel(5)
+            self.setAngel(5)
         elif pygame.key.get_pressed()[pygame.K_RIGHT]:
-                self.setAngel(-5)
-                print("hello!")
-          #      print(self.angel)
-             #   self.image = pygame.image.load('light2.gif')
+            self.setAngel(-5)
+            print("hello!")
+            # print(self.angel)
+            #   self.image = pygame.image.load('light2.gif')
         elif pygame.key.get_pressed()[pygame.K_UP]:
-                self.speedup()
-              #  self.image = pygame.image.load('light1.gif')
+            self.speedup()
+            # self.image = pygame.image.load('light1.gif')
 
-        if self.angel < 90:
-            print("<90")
-        elif self.angel < 180 :
-            print("<180")
-        elif self.angel < 270 :
-            print("<270")
-        elif self.angel < 360 :
-            print("<360")
+        # if self.angel < 90:
+        #            print("<90")
+        #        elif self.angel < 180 :
+        #           print("<180")
+        #      elif self.angel < 270 :
+        #           print("<270")
+        #       elif self.angel < 360 :
+        #           print("<360")
 
-
-        self.x = self.speed*math.cos(math.radians(self.angel))
-        self.y = self.speed*math.sin(math.radians(self.angel))
+        self.x = self.speed * math.cos(math.radians(self.angel))
+        self.y = self.speed * math.sin(math.radians(self.angel))
         self.rect.centery += self.y
         self.rect.centerx += self.x
-    #    print(self.rect.centerx)
-        #print(self.y)
+        # print(self.rect.centerx)
+        print("y:", self.y)
+        print("x:", self.y)
 
 
-    def setAngel(self,direction):
+    def setAngel(self, direction):
         self.angel += direction
         self.angel %= 360
 
-    #    print(self.angel)
-       # self.x += self.speed*math.cos(self.angel)
-       # self.y += self.speed*math.sin(self.angel)
+        # print(self.angel)
+        # self.x += self.speed*math.cos(self.angel)
+        # self.y += self.speed*math.sin(self.angel)
 
     def slowdown(self):
         self.speed -= 1
+
     def speedup(self):
         self.speed += 1
 
+    def createWall():
+        global boundry
+        boundry=[]
+        for i in range(0,Window_Width):
+            boundry.append((i,0))
+            boundry.append((i,Window_Height-18))
+        for i in range(0,Window_Height):
+            boundry.append((0,i))
+            boundry.append((Window_Width-18,i))
 
-        
-    
-        
-        
+    def drawWall():
+        for each in boundry:
+            wallRect = pygame.Rect(each[0],each[1], Cell_Size-2, Cell_Size-2)
+            pygame.draw.rect(DISPLAYSURF, DARKGreen, wallRect)
 def main():
     lb = LightBike()
     Sprites = pygame.sprite.Group(lb)
@@ -99,15 +110,15 @@ def main():
         if lb.rect.centerx <= 0:
             lb.rect.centerx = 0
         bgi = pygame.image.load('BlackBackground.gif')
-        screen.blit(bgi,(0,0))
-        
+        screen.blit(bgi, (0, 0))
+
         Sprites.update()
         Sprites.draw(screen)
         pygame.display.flip()
-    
-    #return mouse cursor
+
+    # return mouse cursor
     pygame.mouse.set_visible(True)
-    
+
 
 if __name__ == "__main__":
     main()
