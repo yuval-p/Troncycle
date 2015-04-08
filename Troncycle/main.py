65469ec2adb09b1c36a94fa391123cb74bf8808c
yuval-p <p.yuval@gmail.com>
2015-04-08 03:30:51 +0300

    1. fixed the control 2. added collision detection 3. added two players

--- Troncycle/main.py
+++ Troncycle/main.py
@@ -4,110 +4,87 @@ import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((1000,1003))
+screen = pygame.display.set_mode((1000, 1003))
 
-class LightBike(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('cycle.gif')
-        self.rect = self.image.get_rect()
+
+class Bike():
+    def __init__(self,color1,color2,startx,starty):
+        self.xdir = startx
+        self.ydir = starty
         self.prev_pos = []
         self.speed = 1
-        self.x = 0.0
-        self.y = 0.0
-        self.angel = 0
-        
+        self.angle = 0
+        self.headcolor = color1 #(0, 0, 255)
+        self.bodydcolor = color2 #(255, 0, 0)
+
+    def setAngle(self, direction):
+        self.angle += direction
+        self.angle %= 360
+
+    def findCollision(self,position):
+        if self.prev_pos.__len__()-20 < 0:
+            maxPos = 0
+        else:
+            maxPos = self.prev_pos.__len__()-20
+
+        for p in self.prev_pos[:maxPos]:
+            if abs(abs(p[0])-abs(position[0])) < 5 and abs(abs(p[1])-abs(position[1])) < 5:
+                print("collision!!!!")
+
+
     def update(self):
+        self.prev_pos.append((self.xdir,self.ydir))
+        if len(self.prev_pos) > 4:
+            pygame.draw.lines(screen, self.headcolor, False, self.prev_pos,7)
+            #make the head of the snake, TODO
+            line = pygame.draw.lines(screen, self.bodydcolor, False, self.prev_pos[:self.prev_pos.__len__()-3],7)
 
-        self.prev_pos.append(self.rect.center)
-        if len(self.prev_pos) > 1:
-            pygame.draw.aalines(screen,(0,0,255),False,self.prev_pos)
-             
-        #if len(self.prev_pos) > 100:
-         #   self.prev_pos.pop(0)
-            
-    #    if pygame.key.get_pressed()[pygame.K_RIGHT]
-                
-        self.image = pygame.image.load('cycle.gif')
-                    
+        self.findCollision((self.xdir,self.ydir))
+
+
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            self.setAngle(-5)
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            self.setAngle(5)
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            if self.speed < 10:
+                self.speed += 1
         if pygame.key.get_pressed()[pygame.K_DOWN]:
-                self.slowdown()
-            #    self.image = pygame.image.load('light4.gif')
-        elif pygame.key.get_pressed()[pygame.K_LEFT]:
-                self.setAngel(5)
-        elif pygame.key.get_pressed()[pygame.K_RIGHT]:
-                self.setAngel(-5)
-                print("hello!")
-          #      print(self.angel)
-             #   self.image = pygame.image.load('light2.gif')
-        elif pygame.key.get_pressed()[pygame.K_UP]:
-                self.speedup()
-              #  self.image = pygame.image.load('light1.gif')
+            if self.speed > 1:
+                self.speed -= 1
 
-        if self.angel < 90:
-            print("<90")
-        elif self.angel < 180 :
-            print("<180")
-        elif self.angel < 270 :
-            print("<270")
-        elif self.angel < 360 :
-            print("<360")
+        self.xdir += self.speed * math.cos(math.radians(self.angle))
+        self.ydir += self.speed * math.sin(math.radians(self.angle))
 
 
-        self.x = self.speed*math.cos(math.radians(self.angel))
-        self.y = self.speed*math.sin(math.radians(self.angel))
-        self.rect.centery += self.y
-        self.rect.centerx += self.x
-    #    print(self.rect.centerx)
-        #print(self.y)
 
 
-    def setAngel(self,direction):
-        self.angel += direction
-        self.angel %= 360
-
-    #    print(self.angel)
-       # self.x += self.speed*math.cos(self.angel)
-       # self.y += self.speed*math.sin(self.angel)
-
-    def slowdown(self):
-        self.speed -= 1
-    def speedup(self):
-        self.speed += 1
-
-
-        
-    
-        
-        
 def main():
-    lb = LightBike()
-    Sprites = pygame.sprite.Group(lb)
+    lb1 = Bike((0, 0, 255),(255, 0,0),50,50)
+    lb2 = Bike((255, 0,0),(0, 0, 255), 500,500)
     clock = pygame.time.Clock()
     while True:
-        clock.tick(500)
+        clock.tick(20)
         pygame.mouse.set_visible(False)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
-        if lb.rect.centery >= screen.get_width():
-            lb.rect.centery = screen.get_width()
-        if lb.rect.centerx >= screen.get_width():
-            lb.rect.centerx = screen.get_width()
-        if lb.rect.centery <= 0:
-            lb.rect.centery = 0
-        if lb.rect.centerx <= 0:
-            lb.rect.centerx = 0
+        if lb1.xdir >= screen.get_width():
+          print("collision!")
+        if lb1.ydir >= screen.get_width():
+          print("collision!")
+
         bgi = pygame.image.load('BlackBackground.gif')
-        screen.blit(bgi,(0,0))
-        
-        Sprites.update()
-        Sprites.draw(screen)
+        screen.blit(bgi, (0, 0))
+
+        lb1.update()
+        lb2.update()
+    #    Sprites.draw(screen)
         pygame.display.flip()
-    
-    #return mouse cursor
+
+    # return mouse cursor
     pygame.mouse.set_visible(True)
-    
+
 
 if __name__ == "__main__":
     main()
